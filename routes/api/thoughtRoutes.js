96fcb7d29c8f6f965ad06492cd0bf6596bba2745
Nathan Moon <moonman700@gmail.com>
2023-06-01 12:30:36 -0400

    added GET all routes for users and thoughts

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const thought = require('../../models/thought')
+
+router.get('/', (req, res) => {
+    thought.find()
+        .then((thoughts) => res.json(thoughts))
+        .catch((err) => res.status(500).json(err))
+}) 
+
+module.exports = router
